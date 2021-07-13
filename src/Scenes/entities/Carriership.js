2f6975dc8794421c4dee31b4307fc251b449d745
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-07-13 13:18:22 +0100

    add callback to Carriership.js

---
+++ src/Scenes/entities/Carriership.js
@@ -0,0 +1,28 @@
+import Phaser from 'phaser';
+import Entity from './Entities';
+import EnemyLaser from './Enemylaser';
+
+export default class CarrierShip extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, 'enemy1', 'CarrierShip');
+    this.body.velocity.y = Phaser.Math.Between(50, 100);
+    this.shootTimer = this.scene.time.addEvent({
+      delay: 2000,
+      callback() {
+        const laser = new EnemyLaser(
+          this.scene,
+          this.x,
+          this.y,
+        );
+        laser.setScale(this.scaleX);
+        this.scene.enemyLasers.add(laser);
+      },
+      callbackScope: this,
+      loop: true,
+    });
+    this.states = {
+      MOVE_DOWN: 'MOVE_DOWN',
+      CHASE: 'CHASE',
+    };
+    this.state = this.states.MOVE_DOWN;
+  }
