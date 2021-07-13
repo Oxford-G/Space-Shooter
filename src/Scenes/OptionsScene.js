6b27925c84f9f74385af8768606a4342d79e75b5
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-07-13 14:11:12 +0100

    add updateAudio func

--- src/Scenes/OptionsScene.js
+++ src/Scenes/OptionsScene.js
@@ -34,5 +34,23 @@ export default class OptionsScene extends Phaser.Scene {
     this.updateAudio();
   }
 
+  updateAudio() {
+    if (this.model.musicOn === false) {
+      this.musicButton.setTexture('box');
+      this.sys.game.globals.bgMusic.stop();
+      this.model.bgMusicPlaying = false;
+    } else {
+      this.musicButton.setTexture('checkedBox');
+      if (this.model.bgMusicPlaying === false) {
+        this.sys.game.globals.bgMusic.play();
+        this.model.bgMusicPlaying = true;
+      }
+    }
 
+    if (this.model.soundOn === false) {
+      this.soundButton.setTexture('box');
+    } else {
+      this.soundButton.setTexture('checkedBox');
+    }
+  }
 }
