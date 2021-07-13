f47e23b91d307a18889e98f7cb2533ef9bcc0a14
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-07-13 17:35:22 +0100

    add apiData.js

--- src/apiData.js
+++ src/apiData.js
@@ -28,6 +28,33 @@ async function postData(inputName) {
   }
 }
 
+const sortPlayers = (input) => {
+  const arr = [];
 
+  for (let i = 0; i < input.length; i += 1) {
+    arr.push([input[i].user, input[i].score]);
+  }
+
+  arr.sort((a, b) => b[1] - a[1]);
+
+  return arr;
+};
+
+async function getData() {
+  const fetchingURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/scores`;
+  try {
+    const response = await fetch(fetchingURL, {
+      mode: 'cors',
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const sorteds = sortPlayers(jsonResponse.result);
+      return sorteds.slice(0, 10);
+    }
+    throw new Error('Request Failed!');
+  } catch (error) {
+    return error;
+  }
+}
 
 export { postData, getData };
